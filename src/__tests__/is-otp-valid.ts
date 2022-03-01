b602763e006f04e1d7e50ec376f2cf95d7b82bb7
Carsten Daurehøj <arelstone@gmail.com>
2022-03-01 09:35:41 +0100

    🐛 Only allow string when checking OTP

--- src/__tests__/is-otp-valid.ts
+++ src/__tests__/is-otp-valid.ts
@@ -5,12 +5,11 @@ describe('is-otp-valid', () => {
     expect(isOtpValid('john@doe')).toBe(false)
     expect(isOtpValid('12345')).toBe(false)
     expect(isOtpValid('1234567')).toBe(false)
-    expect(isOtpValid(1234567)).toBe(false)
     expect(isOtpValid('')).toBe(false)
   })
 
   it('should pass', () => {
     expect(isOtpValid('123456')).toBe(true)
-    expect(isOtpValid(123456)).toBe(true)
+    expect(isOtpValid('000000')).toBe(true)
   })
 })
--- src/is-opt-valid.ts
+++ src/is-opt-valid.ts
@@ -1,1 +1,1 @@
-export const isOtpValid = (otp: string | number) => `${otp}`.length === 6 && `${otp}`.match(/^[0-9]+$/) !== null
+export const isOtpValid = (otp: string) => otp.length === 6 && otp.match(/^[0-9]+$/) !== null
